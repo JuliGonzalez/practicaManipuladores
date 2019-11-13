56741bd2dd435f594b699f99216f235109ed5dbd
Julian Gonzalez <julian.gonzalez@the-cocktail.com>
2019-11-13 15:43:13 +0100

    Upgrading the matrix of the Jacobiana class

--- Jacobiana.py
+++ Jacobiana.py
@@ -8,29 +8,23 @@ import math
 
 
 class Jacobiana:
-    def __init__(self, a1, a2, a3):
+    def __init__(self, a1):
         self.a1 = a1
-        self.a2 = a2
-        self.a3 = a3
 
     def matriz_jacobiana(self, a, b, c):
         return np.array([
             # primera fila
-            [c * math.sin(a) * math.cos(b) - c * math.cos(a) * math.sin(b) - self.a2 * math.sin(a) * math.cos(
-                b) - self.a2 * math.cos(a) * math.sin(b) - self.a1 * math.sin(a),
-             -c * math.cos(a) * math.sin(b) - c * math.sin(a) * math.cos(b) - self.a2 * math.cos(a) * math.sin(
-                 b) - self.a2 * math.sin(a) * math.cos(b),
+            [-c * math.sin(a) * math.cos(b) - c * math.cos(a) * math.sin(b) - self.a1 * math.sin(a),
+             -c * math.cos(a) * math.sin(b) - c * math.sin(a) * math.cos(b),
              math.cos(a) * math.cos(b) - math.sin(a) * math.sin(b)],
             # segunda fila
-            [c * math.cos(a) * math.sin(b) - c * math.sin(a) * math.sin(b) + self.a2 * math.cos(a) * math.cos(
-                b) - self.a2 * math.sin(a) * math.sin(b) + self.a1 * math.cos(a),
-             c * math.sin(a) * math.cos(b) + c * math.cos(a) * math.cos(b) - self.a2 * math.sin(a) * math.sin(
-                 b) + self.a2 * math.cos(a) * math.cos(b),
-             math.sin(a) * math.sin(b) + math.cos(a) * math.sin(b)],
+            [-c * math.cos(a) * math.cos(b) - c * math.sin(a) * math.sin(b) + self.a1 * math.cos(a),
+             -c * math.sin(a) * math.sin(b) + c * math.cos(a) * math.cos(b),
+             math.sin(a) * math.cos(b) + math.cos(a) * math.sin(b)],
             [0, 0, 0],
-            [1, 1, 1],
             [0, 0, 0],
-            [0, 0, 0]
+            [0, 0, 0],
+            [1, 1, 1]
         ])
 
     def velocidades(self, a, b, c):
@@ -45,12 +39,12 @@ class Jacobiana:
 
 
 if __name__ == "__main__":
-    jacobiana1 = Jacobiana(0.5, 0.5, 0)
-    jacobiana2 = Jacobiana(1, 1, 0)
-    caso1 = np.array([0, pi/90, 0])
-    caso2 = np.array([-pi / 90, pi / 90, 0])
-    caso3 = np.array([pi / 90, -pi / 90, -200/100])  # introducimos un valor determinado para a
-    caso4 = np.array([pi / 90, 0, -200/100])  # introducimos un valor determinado para a
+    jacobiana1 = Jacobiana(0.5)
+    jacobiana2 = Jacobiana(4)
+    caso1 = np.array([0, np.pi/90, 0])
+    caso2 = np.array([-np.pi / 90, np.pi / 90, 0])
+    caso3 = np.array([np.pi / 90, -np.pi / 90, -200/100])  # introducimos un valor determinado para a
+    caso4 = np.array([np.pi / 90, 0, -200/100])  # introducimos un valor determinado para a
     m1 = jacobiana2.matriz_jacobiana(np.pi/4, -np.pi/4,  0)
     # print(m1)
     m2 = jacobiana2.velocidades(-np.pi/90, np.pi/90, 0)
