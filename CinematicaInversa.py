d8602e24a3fbd86f74e7f6e0d264aca7be2b58fb
Julian Gonzalez Dos Reis <Julian@MacBook-Air-de-Julian.local>
2019-11-15 16:05:23 +0100

    Adding variables into Inverse kinematics class

--- CinematicaInversa.py
+++ CinematicaInversa.py
@@ -21,5 +21,27 @@ if __name__ == "__main__":
     print(simplify(suma))
 
 
+    part1 = (-d * sin(t1) * cos(t2) - d * cos(t1) * sin(t2) - a * sin(t1)) * (
+            -d * sin(t1) * sin(t2) + d * cos(t1) * cos(t2)) + (
+                    -d * cos(t1) * sin(t2) - d * sin(t1) * cos(t2)) * (sin(t1) * cos(t2) + cos(t1) * sin(t2)) + (
+                    -d * cos(t1) * cos(t2) - d * sin(t1) * sin(t2) - a * cos(t1)) * (
+                    cos(t1) * cos(t2) - sin(t1) * sin(t2))
+
+    part2 = -(-d * sin(t1) * sin(t2) + d * cos(t1) * cos(t2)) * (cos(t1) * cos(t2) - sin(t1) * sin(t2)) - (
+                -d * sin(t1) * cos(t2) - d * cos(t1) * sin(t2) - a * sin(t1)) * (
+                        sin(t1) * cos(t2) + cos(t1) * sin(t2)) -(-d * cos(t1) * sin(t2) - d * sin(t1) * cos(t2)) * (
+                        d * cos(t1) * cos(t2) - d * sin(t1) * sin(t2) + a + cos(t1))
+    jacobiana = part1 + part2
+    # print(jacobiana)
+    jacobiana_simplified = simplify(jacobiana)
+    print(jacobiana_simplified)
+    expanded = sympy.factor(jacobiana)
+    factorized = sympy.factor(jacobiana)
+    print(expanded)
+    print(factorized)
+    trigsim = sympy.trigsimp(jacobiana)
+    print(trigsim)
+
+
     # print(part1)
     # print(simplify(part1))
